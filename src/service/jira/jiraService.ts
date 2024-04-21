7bac3155e337deead423c7b1bb0ed4ca6abf81ff
mmaleszewski <mmaleszewski@trans.eu>
2024-04-21 18:53:21 +0200

    Add Jira service file for logging time and updating Jira issues

---
+++ src/service/jira/jiraService.ts
@@ -0,0 +1,104 @@
+import JiraApi from "jira-client";
+import { isWorkday } from "../../utils";
+
+export interface JiraTask {
+  date: Date;
+  jiraTaskId: string;
+  comment: string;
+  timeSpent: string;
+}
+if (!process.env.JIRA_HOST || !process.env.JIRA_BEARER) {
+  throw new Error("Missing env variables");
+}
+
+const jira = new JiraApi({
+  protocol: "https",
+  host: process.env.JIRA_HOST,
+  bearer: process.env.JIRA_BEARER,
+  apiVersion: "2",
+  strictSSL: true,
+});
+
+const formatJiraDate = (date: Date): string => {
+  return `${date.getFullYear()}-${
+    date.getMonth() + 1
+  }-${date.getDate()}T${date.getHours()}:${date.getMinutes()}:00.000+0000`;
+};
+
+export const logJiraTime = async ({
+  date,
+  jiraTaskId,
+  comment,
+  timeSpent,
+}: JiraTask): Promise<JiraApi.JsonResponse | undefined> => {
+  try {
+    const jiraLogDate = new Date(date);
+    jiraLogDate.setHours(9, 40, 0, 0);
+
+    return await jira.addWorklog(jiraTaskId, {
+      comment,
+      started: formatJiraDate(jiraLogDate),
+      timeSpent,
+    });
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const loopDays = async ({
+  startDate,
+  endDate,
+  jiraTaskId,
+  comment,
+  timeSpent,
+}: {
+  startDate: Date;
+  endDate: Date;
+  jiraTaskId: string;
+  comment: string;
+  timeSpent: string;
+}): Promise<void> => {
+  const currentDate = new Date(startDate);
+  const loopEndDate = new Date(endDate);
+
+  // eslint-disable-next-line no-unmodified-loop-condition
+  while (currentDate <= loopEndDate) {
+    if (isWorkday(currentDate)) {
+      await logJiraTime({
+        date: currentDate,
+        jiraTaskId,
+        comment,
+        timeSpent,
+      });
+    } else {
+      console.log("free");
+    }
+    currentDate.setDate(currentDate.getDate() + 1);
+  }
+};
+
+export const getJiraTask = async (
+  jiraTaskId: string
+): Promise<JiraApi.JsonResponse> => {
+  return await jira.findIssue(jiraTaskId);
+};
+
+export const updateJiraIssue = async (
+  jiraTaskId: string,
+  data: any
+): Promise<JiraApi.JsonResponse> => {
+  return await jira.updateIssue(jiraTaskId, data);
+};
+export const getCurrentJiraUser = async (): Promise<JiraApi.JsonResponse> => {
+  return await jira.getCurrentUser();
+};
+// jira.getCurrentUser;
+// jira.getIssue;
+// jira.searchJira;
+// jira.getSprint;
+// jira.getUsersInGroup;
+// jira.getUsersIssues;
+// jira.updateIssue;
+
+export default jira;
