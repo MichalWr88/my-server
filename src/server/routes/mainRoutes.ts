64240217955327ef4d38aba2f739eb2460b42076
mmaleszewski <mmaleszewski@trans.eu>
2024-04-21 18:35:20 +0200

    Add index.html and mainRoutes.ts files

---
+++ src/server/routes/mainRoutes.ts
@@ -0,0 +1,19 @@
+import packageFile from "../../../package.json";
+import { FastifyInstance } from "fastify";
+import { createRoute } from "./routeUtils";
+const path = require('node:path')
+export const mainRoutes = async (server: FastifyInstance) => {
+  createRoute(server, "/token", (req, reply) => {
+    const token = req.jwt.sign({ hello: "world" });
+    reply.send({
+      hello: "22",
+      host: process.env.JIRA_HOST,
+      v: packageFile.version,
+      token,
+    });
+  });
+  createRoute(server, "/", (req, reply) => {
+   
+   reply.sendFile('index.html')
+  });
+};
