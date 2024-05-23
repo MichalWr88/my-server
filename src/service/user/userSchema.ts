a3e86625da3e2e8b242140fdafedff5af1622962
mmaleszewski <mmaleszewski@trans.eu>
2024-05-23 08:24:54 +0200

    chore: Update userSchema.ts to adhere to code style guidelines

--- src/service/user/userSchema.ts
+++ src/service/user/userSchema.ts
@@ -1,43 +1,45 @@
-import { z } from 'zod'
-import { buildJsonSchemas } from 'fastify-zod'
+import { z } from "zod";
+import { buildJsonSchemas } from "fastify-zod";
 
 // data that we need from user to register
 const createUserSchema = z.object({
-    email: z.string(),
-    password: z.string().min(6),
-    name: z.string(),
-    type: z.enum(['local', 'prod']),
-  })
-  //exporting the type to provide to the request Body
-  export type CreateUserInput = z.infer<typeof createUserSchema>
-  // response schema for registering user
-  const createUserResponseSchema = z.object({
-    id: z.string(),
-    email: z.string(),
-    name: z.string(),
-    type: z.enum(['local', 'prod']),
-  })
-  
-  // same for login route
-  const loginSchema = z.object({
-    email: z
-      .string({
-        required_error: 'Email is required',
-        invalid_type_error: 'Email must be a string',
+  email: z.string(),
+  password: z.string().min(6),
+  name: z.string(),
+  type: z.enum(["local", "prod"]),
+});
+//exporting the type to provide to the request Body
+export type CreateUserInput = z.infer<typeof createUserSchema>;
+// response schema for registering user
+const createUserResponseSchema = z.object({
+  id: z.string(),
+  email: z.string(),
+  name: z.string(),
+  type: z.enum(["local", "prod"]),
+});
 
-      })
-      .email(),
-    password: z.string().min(6),
-    type: z.enum(['local', 'prod']),
-  })
-  export type LoginUserInput = z.infer<typeof loginSchema>
-  const loginResponseSchema = z.object({
-    accessToken: z.string(),
-  })
+// same for login route
+const loginSchema = z.object({
+  email: z
+    .string({
+      required_error: "Email is required",
+      invalid_type_error: "Email must be a string",
+    })
+    .email(),
+  password: z.string().min(6),
+  type: z.enum(["local", "prod"]),
+});
+export type LoginUserInput = z.infer<typeof loginSchema>;
+const loginResponseSchema = z.object({
+  accessToken: z.string(),
+});
 
-  export const { schemas: userSchemas, $ref } = buildJsonSchemas({
+export const { schemas: userSchemas, $ref } = buildJsonSchemas(
+  {
     createUserSchema,
     createUserResponseSchema,
     loginSchema,
     loginResponseSchema,
-  })
+  },
+  { $id: "userSchema" }
+);
