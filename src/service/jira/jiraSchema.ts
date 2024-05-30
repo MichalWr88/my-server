3ca32b9d7030591cb75673cbdbcb4b006bb18ba3
mmaleszewski <mmaleszewski@trans.eu>
2024-05-30 19:51:56 +0200

    chore: Update Jira schema and add JSON schemas for Jira service

--- src/service/jira/jiraSchema.ts
+++ src/service/jira/jiraSchema.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 import { buildJsonSchemas } from "fastify-zod";
-
+//
 const JiraTaskSchemaRequest = z.object({
   date: z.date(),
   jiraTaskId: z.string(),
@@ -8,6 +8,52 @@ const JiraTaskSchemaRequest = z.object({
   timeSpent: z.string(),
 });
 export type JiraTaskRequest = z.infer<typeof JiraTaskSchemaRequest>;
+//
+
+const JiraOfMonthSchemaRequest = z.object({
+  user: z.string(),
+  type: z.literal("M"),
+  prevStart: z.number().optional(),
+  prevEnd: z.number().optional(),
+});
+const JiraOfDaySchemaRequest = z.object({
+  user: z.string(),
+  type: z.literal("D"),
+  prevStart: z.number().optional(),
+  prevEnd: z.number().optional(),
+});
+const JiraOfWeekSchemaRequest = z.object({
+  user: z.string(),
+  type: z.literal("W"),
+  prevStart: z.number().optional(),
+  prevEnd: z.number().optional(),
+});
+
+export type JiraOfMonth = z.infer<typeof JiraOfMonthSchemaRequest>;
+export type JiraOfDay = z.infer<typeof JiraOfDaySchemaRequest>;
+export type JiraOfWeek = z.infer<typeof JiraOfWeekSchemaRequest>;
+
+const JiraQueryDatesSchemaRequest = z.union([
+  JiraOfDaySchemaRequest,
+  JiraOfWeekSchemaRequest,
+  JiraOfMonthSchemaRequest,
+]);
+export type JiraWorklogByTimeRequest = z.infer<
+  typeof JiraQueryDatesSchemaRequest
+>;
+
+const JiraWorklogPreConfiguredSchemaRequest = z.object({
+  type: z.union([
+    z.literal("currentMonth"),
+    z.literal("currentWeek"),
+    z.literal("currentDay"),
+    z.literal("yesterday"),
+    z.literal("lastWeek"),
+    z.literal("lastMonth"),
+  ]),
+  user: z.string(),
+});
+export type JiraWorklogPreConfiguredRequest = z.infer<typeof JiraWorklogPreConfiguredSchemaRequest>;
 
 const JiraLoopDaysSchemaRequest = z.object({
   startDate: z.date(),
@@ -27,6 +73,8 @@ export const { schemas: jiraSchemas, $ref } = buildJsonSchemas(
     JIraTaskIdSchema,
     JiraLoopDaysSchemaRequest,
     JiraTaskSchemaRequest,
+    JiraQueryDatesSchemaRequest,
+    JiraWorklogPreConfiguredSchemaRequest,
   },
   { $id: "jiraSchema" }
 );
