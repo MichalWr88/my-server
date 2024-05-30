7f6cfc1d3825f209086d7aabc514a7bf0ec13114
mmaleszewski <mmaleszewski@trans.eu>
2024-05-30 20:02:59 +0200

    chore: Refactor Jira log-time endpoint URL

---
+++ src/service/jira/jiraController.ts
@@ -0,0 +1,98 @@
+import JiraApi from "jira-client";
+import { FastifyReply, FastifyRequest } from "fastify";
+import {
+  JiraLoopDaysRequest,
+  JiraTaskRequest,
+  JiraWorklogByTimeRequest,
+  JiraWorklogPreConfiguredRequest,
+} from "./jiraSchema";
+import {
+  addJiraWorklog,
+  getJiraWorklogByTime,
+  loopDays,
+  mapPreConfiguredDates,
+} from "./jiraService";
+export const logJiraTime = async (
+  req: FastifyRequest<{
+    Body: JiraTaskRequest;
+  }>,
+  reply: FastifyReply
+): Promise<JiraApi.JsonResponse | undefined> => {
+  try {
+    console.log(req.body);
+    const { comment, date, jiraTaskId, timeSpent } = req.body;
+    const jiraResp = await addJiraWorklog({
+      comment,
+      date,
+      jiraTaskId,
+      timeSpent,
+    });
+    return reply.code(200).send(jiraResp);
+  } catch (e) {
+    return reply.code(500).send(e);
+  }
+};
+
+export const logJiraLoopDays = async (
+  req: FastifyRequest<{
+    Body: JiraLoopDaysRequest;
+  }>,
+  reply: FastifyReply
+): Promise<JiraApi.JsonResponse | undefined> => {
+  try {
+    const { comment, startDate, endDate, jiraTaskId, timeSpent } = req.body;
+    const jiraResp = await loopDays({
+      comment,
+      startDate,
+      endDate,
+      jiraTaskId,
+      timeSpent,
+    });
+    console.log(jiraResp);
+    return reply.code(200).send({ message: "success" });
+  } catch (e) {
+    return reply.code(500).send(e);
+  }
+};
+export const searchJiraQuery = async (
+  req: FastifyRequest<{
+    Body: JiraWorklogByTimeRequest;
+  }>,
+  reply: FastifyReply
+): Promise<JiraApi.JsonResponse | undefined> => {
+  try {
+    const { user, type, prevStart, prevEnd } = req.body;
+    const jiraResp = await getJiraWorklogByTime({
+      user,
+      type,
+      prevStart,
+      prevEnd,
+    });
+    console.log(jiraResp);
+    return reply.code(200).send(jiraResp);
+  } catch (e) {
+    return reply.code(500).send(e);
+  }
+};
+export const searchJiraQueryPreConfigured = async (
+  req: FastifyRequest<{
+    Querystring: JiraWorklogPreConfiguredRequest;
+  }>,
+  reply: FastifyReply
+): Promise<JiraApi.JsonResponse | undefined> => {
+  try {
+    const { user, type } = req.query;
+
+    const jiraWorklogRequest = mapPreConfiguredDates({ type, user });
+    const jiraResp = await getJiraWorklogByTime({
+      user: jiraWorklogRequest.user,
+      type: jiraWorklogRequest.type,
+      prevStart: jiraWorklogRequest.prevStart,
+      prevEnd: jiraWorklogRequest.prevEnd,
+    });
+    console.log(jiraResp);
+    return reply.code(200).send(jiraResp);
+  } catch (e) {
+    return reply.code(500).send(e);
+  }
+};
