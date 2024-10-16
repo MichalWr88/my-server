2911647146a721f4b02341980eb4f162c0b0cf72
mmaleszewski <mmaleszewski@trans.eu>
2024-10-16 19:17:56 +0200

    feat: Add mikrusRoutes to handle Mikrus API requests
    This commit adds a new file, mikrusRoutes.ts, which contains the implementation for handling Mikrus API requests. The mikrusRoutes module is registered in the setRouting function in routing.ts, with the prefix "api/mikrus". This allows the server to handle routes related to Mikrus API calls.
    
    The mikrusRoutes module defines several routes for different Mikrus API endpoints, such as /info, /serwery, /restart, /logs, /amfetamina, /db, /exec, /stats, /porty, /cloud, and /domain. Each route makes use of the mikrusRequest function to send requests to the Mikrus API and return the response.
    
    This addition enables the server to handle Mikrus API requests and provides a convenient way to interact with the Mikrus API from the server.

--- src/server/routes/routing.ts
+++ src/server/routes/routing.ts
@@ -3,9 +3,11 @@ import { FastifyInstance } from "fastify";
 import { jiraRoutes } from "./jiraRoutes";
 import { mainRoutes } from "./mainRoutes";
 import { userRoutes } from "./userRoutes";
+import { mikrusRoutes } from "./mikrusRoutes";
 
 export const setRouting = (server: FastifyInstance) => {
   server.register(mainRoutes);
   server.register(jiraRoutes, { prefix: "api/jira" });
   server.register(userRoutes, { prefix: "api/user" });
+  server.register(mikrusRoutes, { prefix: "api/mikrus" });
 };
